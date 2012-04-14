3ec3b8e1b4a040ff7ba6e464f5e8c01dfdff858f
alfredoscoppa <alfredo.scoppa@hotmail.com>
2012-04-13 22:56:07 -0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gym::Application.config.secret_token = '96125d6355bcef8eb12f7c3a68fc7aeb28dc8857a5c7ca12d460fb33c4f4b8a3d6ac634c8c8e18d3f09a471b42368524293c48704864c60b040f37311f9bd584'
